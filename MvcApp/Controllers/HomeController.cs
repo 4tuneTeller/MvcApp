df7a5f0cd9470e220366dcf4e231cc463bbc924b
Насыров Тимур <timmy.nas@gmail.com>
2015-09-05 23:04:42 +0500

    Improved query

--- MvcApp/Controllers/HomeController.cs
+++ MvcApp/Controllers/HomeController.cs
@@ -17,12 +17,16 @@ namespace MvcApp.Controllers
             Array contacts;
             using (AddressBookEntities context = new AddressBookEntities())
             {
-                var query = from c in context.AddressBook
-                            group c by c.Department into departments
-                            orderby departments.Key
-                            select departments;
+                //var query = from c in context.AddressBook
+                //            group c by c.Department into departments
+                //            orderby departments.Key
+                //            select departments;
 
-                contacts = query.ToArray();
+                //select new GroupedContacts { Group = departments.Key, Contacts = departments.OrderBy(c => c.FullName).ToArray() }; //{ Group = departments.Key, Elements = departments.OrderBy(c => c.FullName) };
+
+                //contacts = query.AsEnumerable().Select(g => new GroupedContacts { Group = g.Key, Contacts = g.OrderBy(c => c.FullName).ToArray() }).ToArray();
+                
+                contacts = context.AddressBook.GroupBy(c => c.Department).AsEnumerable().Select(g => new GroupedContacts { Group = g.Key, Contacts = g.OrderBy(c => c.FullName).ToArray() }).ToArray();
             }
 
             return View(contacts);
@@ -49,6 +53,18 @@ namespace MvcApp.Controllers
             {
                 ViewBag.Message = "Неверная комбинация логина и пароля";
                 return View();
+            }
+        }
+
+        public struct GroupedContacts
+        {
+            public string Group;
+            public AddressBook[] Contacts;
+
+            public GroupedContacts(string group, AddressBook[] contacts)
+            {
+                Group = group;
+                Contacts = contacts;
             }
         }
     }
